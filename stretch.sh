f51123e8f8657172ab66afdb36d6cbbe7d5edfc1
Matej Kollar <208115@mail.muni.cz>
2024-07-01 08:46:15 +0100

    Modify stretching script...

--- stretch.sh
+++ stretch.sh
@@ -2,14 +2,31 @@
 
 set -eu
 
-readonly x=${1?X}
-readonly y=${2?Y}
-readonly r=${3?R}
-readonly d=${4?D}
+x=0
+y=0
+r=1
+d=0.5
+
+args=()
+
+while [[ ${#} -ne 0 ]]; do
+    opt=${1}; shift
+    case "${opt}" in
+        -x) x=${1?VALUE}; shift;;
+        -y) y=${1?VALUE}; shift;;
+        -r) r=${1?VALUE}; shift;;
+        -d) d=${1?VALUE}; shift;;
+        --) break;;
+        -*) echo "Unknown arg: ${opt}"; exit 1;;
+        *) args=( "${opt}" "${@}" )
+    esac
+done
 
 exec awk \
     --assign="X=${x}" \
     --assign="Y=${y}" \
     --assign="R=${r}" \
     --assign="D=${d}" \
-    '/vertex/{f=1+D/(1+(1/R)*(($2-X)**2+($3-Y)**2));$2=$2*f;$3=$3*f}{print}'
+    -- \
+    '/vertex/{f=D/(1+(1/R)*(($2-X)**2+($3-Y)**2));$2=$2+($2-X)*f;$3=$3+($3-Y)*f}{print}' \
+    "${args[@]}"
